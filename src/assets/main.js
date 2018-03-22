b7a3e4119e7e360b70beb8d3b67538f7c4b404de
Reggie Smith <regsmit@cisco.com>
2018-03-22 14:06:27 -0600

    my stuff

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,42 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+      url: 'https://www.codeschool.com/users/4577578.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        addCourses(response.courses.completed);
+      }
+  });
 
+  function addCourses(courses) {
+
+      var $badges = $('#badges');
+
+      courses.forEach(function(course) {
+
+          var $course = $('<div />', {
+              'class': 'course'
+          }).appendTo($badges);
+
+          $('<h3 />', {
+              text: course.title
+          }).appendTo($course);
+
+          var $image = $('<img />', {
+              src: course.badge
+          }).appendTo($course);
+
+          $('<a />', {
+              'class': 'btn btn-prmary',
+              target: '_blank',
+              href: course.url,
+              text: 'See Course'
+          }).appendTo($course);
+
+          $($image).wrap($("<a>").attr("href",course.url));
+
+      })
+
+  }
 });
